c6c4d66d2869fc3fdb4ca6a1f971942af88cdc01
Anna <anna.koehler@stthomas.edu>
2019-05-16 12:25:14 -0500

    look into taking the StdIn OUT OF createPlayers class

---
+++ SkunkProject/test/ControllerMethods.java
@@ -0,0 +1,103 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ControllerMethods {
+	Controller c;
+	SkunkDice dice;
+	
+	
+	@Before
+	public void setUp() throws Exception {
+		c = new Controller();
+		dice = new SkunkDice();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		c = null;
+	}
+
+	
+	@Test
+	public void testCreatePlayers() {
+		
+		c.createPlayers(4);
+		int createdListSize = c.playerList.size();
+		
+		assertTrue(createdListSize == 4);
+	}
+
+	@Test
+	public void testNextPlayer() {
+		c.nextPlayer();
+	}
+
+	@Test
+	public void testPlayerTurn() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPlayerTurnContinue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAllPlayersInfo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRollInfo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPlayerTurnEnd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckSkunk() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckHundred() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetFinalTurnFlag() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRoundEnd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetWinner() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPlayerTurns() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGameEnd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testShowRules() {
+		fail("Not yet implemented");
+	}
+
+}
